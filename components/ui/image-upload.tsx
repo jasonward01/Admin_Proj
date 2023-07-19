9f9689f537a745f6d592e07086b24baa80fe4a11
Jason Ward <jason.ward.actual@gmail.com>
2023-07-19 15:08:05 -0500

    final

--- components/ui/image-upload.tsx
+++ components/ui/image-upload.tsx
@@ -53,7 +53,7 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
           </div>
         ))}
       </div>
-      <CldUploadWidget onUpload={onUpload} uploadPreset="t4drjppf">
+      <CldUploadWidget onUpload={onUpload} uploadPreset="e1sx1vhj">
         {({ open }) => {
           const onClick = () => {
             open();
